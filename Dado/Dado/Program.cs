3c9e3cced04521feb842e4042e3145fe4ec099b3
GerardoAc <gera.araya3@gmail.com>
2019-11-30 12:00:37 -0600

    dibujar dado

---
+++ Dado/Dado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            Console.WriteLine("Precione Enter para visualizar ");
+            while (Console.ReadKey().Key == ConsoleKey.Enter) {
+                int n1;
+                Console.WriteLine("nueva visulacion");
+                Console.WriteLine("            +++++++    ");
+                Console.WriteLine("            +     +     ");
+                Console.WriteLine("            +     +     ");
+                Console.WriteLine("            +     +     ");
+                Console.WriteLine("+++++++++++++++++++++++++");
+                Console.WriteLine("+     +     +     +     +");
+                Console.WriteLine("+     +     +     +     +");
+                Console.WriteLine("+     +     +     +     +");
+                Console.WriteLine("+++++++++++++++++++++++++");
+                Console.WriteLine("            +     +   ");
+                Console.WriteLine("            +     +     ");
+                Console.WriteLine("            +     +     ");
+                Console.WriteLine("            +++++++      ");
+            }
+        }
+        }
+    }
+
