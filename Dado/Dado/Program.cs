4aa11ac28bb1f626e8259861a3de7f43259fc18f
GerardoAc <gera.araya3@gmail.com>
2019-12-01 22:47:48 -0600

    funcion para dibujar el dado

--- Dado/Dado/Program.cs
+++ Dado/Dado/Program.cs
@@ -14,13 +14,32 @@ namespace Dado
                 Console.WriteLine("nueva visulacion");
                 Program dado = new Program();
                 int[] numeros=new int[3];
-                
-               
+
+                dado.dibujar(4,1,5);
+
+
+                    
              
             }
         }
-      
-        
+        private void dibujar(int n0, int n1, int n2)
+        {
+
+            Console.WriteLine("                ##########         ");
+            Console.WriteLine("                #        #         ");
+            Console.WriteLine("                #   {0}    #         ", n0);
+            Console.WriteLine("                #        #         ");
+            Console.WriteLine("###################################");
+            Console.WriteLine("#      #        #        #        #");
+            Console.WriteLine("# {0}    #    {1}   #    {2}   #    {3}   #", 7 - n1, 7 - n2, n1, n2);
+            Console.WriteLine("#      #        #        #        #");
+            Console.WriteLine("###################################");
+            Console.WriteLine("                #        #         ");
+            Console.WriteLine("                #   {0}    #         ", 7 - n0);
+            Console.WriteLine("                #        #         ");
+            Console.WriteLine("                ##########         ");
         }
+
+    }
     }
 
